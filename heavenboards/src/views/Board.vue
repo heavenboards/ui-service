ef06a0944b65d9add88e30a60c8d13a7421f3812
zozulya.michael <zozymisha@gmail.com>
2024-03-07 21:16:43 +0300

    separating styles from views

--- heavenboards/src/views/Board.vue
+++ heavenboards/src/views/Board.vue
@@ -12,7 +12,7 @@
     <div class="board-name">board_name</div>
     <div class="board">
         <div class="board-content">
-            <div class="column-wrapper">  
+            <div class="column-wrapper">
                 <div class="column">
                     <div class="title">column_name</div>
                     <hr>
@@ -34,104 +34,7 @@
         </div>
     </div>
 </template>
+
 <style>
-    .menu{
-        height: 100px;
-        border-style: solid;
-        border-width: 0 0 3px 0;
-        border-color: var(--hb-c-green);
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-    }
-
-    .menu-content{
-        padding: 20px 50px;
-        display: flex;
-    }
-
-    .menu-content button{
-        margin: 0 0 0 50px;
-        padding: 15px 40px;   
-        text-transform: uppercase; 
-    }
-    .board-name{
-        padding: 20px 30px;
-        font-size: 2.2em;
-        font-weight: bolder;
-    }
-    .board-content{
-        padding: 20px 30px;
-        display: flex;
-    }
-    
-    .board-content .column{
-        width: 300px;
-        padding: 30px 20px;
-        border-radius: 10px;
-        border-style: solid;
-        border-width: 0;
-        background-color: var(--hb-c-gray);
-    }
-
-    .column-wrapper{
-        margin: 0 0 0 50px;
-    }
-
-    .column-wrapper:first-of-type{
-        margin: 0 !important;
-    }
-
-    .column-add{
-        width: 300px;
-        height: 30px;
-        border-radius: 10px;
-        border-style: solid;
-        border-width: 0;
-        background-color: var(--hb-c-gray);
-        border-style: solid;
-        text-transform: uppercase;
-        color: var(--hb-c-green);
-        border-color: var(--hb-c-green);
-        border-radius: 3px;
-        border-width: 1px;
-        transition: 0.2s;
-        margin: 0 0 0 50px;
-    }
-
-    .board-content .title{
-        font-size: 1.1em;
-        font-weight: bold;
-    }
-
-    .column button{
-        width: 100%;
-        height: 30px;
-        text-transform: uppercase;
-        background-color: var(--hb-c-transparent);
-        border-style: solid;
-        color: var(--hb-c-green);
-        border-color: var(--hb-c-green);
-        border-radius: 3px;
-        border-width: 1px;
-        transition: 0.2s;
-    }
-
-    .tasks button{
-        border-radius: 0;
-        border-width: 0 0 0 3px;
-        color: var(--hb-c-white);
-        margin: 0 0 10px 0;
-        text-transform: none;
-    }
-
-    .board-content button:hover{
-        background-color: var(--hb-c-green);
-        color: var(--hb-c-gray);
-    }
-
-    .board-content hr{
-        border-color: var(--hb-c-green);
-        margin: 20px 0;
-    }
+@import "./styles/BoardStyles.css";
 </style>
--- heavenboards/src/views/Login.vue
+++ heavenboards/src/views/Login.vue
@@ -49,7 +49,7 @@
             <div class="switcher">
                 <button v-on:click="on_click_switcher(0)" id="switcher-login" class="active">log in</button>
                 <button v-on:click="on_click_switcher(1)" id="switcher-signup">sign up</button>
-            </div>  
+            </div>
             <div v-if="isLogin" class="auth">
                 <form action="" method="post">
                     <input name="username" type="text" placeholder="username" >
@@ -68,66 +68,8 @@
             </div>
         </div>
     </body>
-</template> 
+</template>
+
 <style>
-    .content{
-        width: 100%;
-        margin: 20% 0;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-    }
-
-    form{
-        display: flex;
-        flex-direction: column;
-    }
-    .switcher{
-        width: 240px;      
-    }
-    .switcher button{
-        width: 50%;
-        height: 35px;
-        text-transform: uppercase;
-        border-radius: 0;
-        border-style: none;
-        background-color: var(--hb-c-gray);
-        color: var(--hb-c-green);
-        transition: 0.2s;
-    }
-    .switcher .active{
-        background-color: var(--hb-c-green);
-        color: var(--hb-c-gray)
-    }
-    input {
-        width: 240px;
-        height: 35px;
-        margin: 10px 0;
-        font-size: 1.1em;
-        border-width: 0 0 0 3px;
-        border-style: solid;
-        border-color: var(--hb-c-green);
-        background-color: var(--hb-c-gray);
-        color: var(--hb-c-white);
-        padding: 5px 10px;
-        outline: none;
-    }
-
-    input::placeholder{
-        color: var(--hb-c-lightgray)
-    }
-
-    input:focus {
-        border-color: var(--hb-c-green);
-    }
-
-    #login, #signup{
-        height: 50px;
-        color: white;
-        background-color: var(--hb-c-green);
-        border-radius: 5px;
-        border-style: none;
-        font-size: 1.2em;
-        text-transform: uppercase;
-    }
+@import "./styles/LoginStyles.css";
 </style>
