d46bcdcf1a07b78c0dc931032d773ff149f62364
zozulya.michael <zozymisha@gmail.com>
2024-03-13 23:15:07 +0300

    Добавлена базовая логика регистрации и аутентификации

--- heavenboards/src/router/index.ts
+++ heavenboards/src/router/index.ts
@@ -3,25 +3,23 @@ import LoginView from '../views/Login.vue'
 import HomeView from '../views/Home.vue'
 import BoardView from '../views/Board.vue'
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/login',
-      name: 'login',
-      component: LoginView
-    },
-    {
-      path: '/home',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/board',
-      name: 'board',
-      component: BoardView
-    }
-  ]
-})
+const routes = [
+  {
+    path: '/home',
+    name: 'Home',
+    component: HomeView,
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: LoginView,
+  },
+    //....
+];
 
-export default router
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+export default router;
--- heavenboards/src/views/Login.vue
+++ heavenboards/src/views/Login.vue
@@ -1,12 +1,39 @@
 <script setup lang="ts">
 import Brand from '../components/Brand.vue'
-import {ref} from "vue";
-
-const isLogin = ref(true);
+import { ref } from "vue";
+import { useRouter } from 'vue-router';
+import apiClient from '@/api/api';
 
 const toggleLoginSignup = () => {
     isLogin.value = !isLogin.value;
-}
+};
+
+const username = ref('');
+const password = ref('');
+const firstName = ref('');
+const lastName = ref('');
+const isLogin = ref(true);
+
+const router = useRouter();
+
+const onSuccessfulAuth = () => {
+    router.push({ name: 'Home' });
+};
+
+const handleSubmit = async () => {
+    let endpoint = isLogin.value ? '/authenticate' : '/register';
+    let payload = isLogin.value ? {username: username.value, password: password.value} :
+                                  {username: username.value, password: password.value, firstName: firstName.value, lastName: lastName.value};
+  try {
+    const response = await apiClient.post(endpoint, payload);
+    localStorage.setItem('authToken', response.data.token);
+    console.log(response);
+    onSuccessfulAuth();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 </script>
 
 <template>
@@ -14,23 +41,23 @@ const toggleLoginSignup = () => {
           <div class="content">
               <Brand/>
               <div class="switcher">
-                  <button :class="{ active: isLogin }" @click="toggleLoginSignup">log in</button>
-                  <button :class="{ active: !isLogin }" @click="toggleLoginSignup">sign up</button>
+                  <button :class="{ active: isLogin }" @click="toggleLoginSignup">Log in</button>
+                  <button :class="{ active: !isLogin }" @click="toggleLoginSignup">Sign up</button>
               </div>
               <div v-if="isLogin" class="auth">
-                  <form method="post" action="/login">
-                      <input name="username" type="text" placeholder="username" required>
-                      <input name="password" type="password" placeholder="password" required>
-                      <input type="submit" value="log in">
+                  <form @submit.prevent="handleSubmit">
+                      <input name="username" type="text" placeholder="Username" required>
+                      <input name="password" type="password" placeholder="Password" required>
+                      <input type="submit" value="Log in">
                   </form>
               </div>
               <div v-else class="registration">
-                  <form method="post" action="/signup">
-                      <input name="name" type="text" placeholder="name" required>
-                      <input name="surname" type="text" placeholder="surname" required>
-                      <input name="username" type="text" placeholder="username" required>
-                      <input name="password" type="password" placeholder="password" required>
-                      <input type="submit" value="sign up">
+                  <form @submit.prevent="handleSubmit">
+                      <input name="username" type="text" placeholder="Username" required>
+                      <input name="password" type="password" placeholder="Password" required>
+                      <input name="firstName" type="text" placeholder="First Name" required>
+                      <input name="lastName" type="text" placeholder="Last Name" required>
+                      <input type="submit" value="Sign up">
                   </form>
               </div>
           </div>
